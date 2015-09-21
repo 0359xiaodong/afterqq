e787dee3c0d2d1889687dd4965d6fd33666c4ce7
spadd <vimchan@qq.com>
2015-09-21 13:53:03 +0800

    加了注释

--- src/com/jieke/qq/activity/BaseActivity.java
+++ src/com/jieke/qq/activity/BaseActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class BaseActivity extends Activity {
-	
+	// 测试，这是BaseActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
