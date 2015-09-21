7a161a05984ef7e2590ddc3d4568358a367e48b5
spadd <vimchan@qq.com>
2015-09-21 13:42:57 +0800

    1111

---
+++ src/com/jieke/qq/activity/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.jieke.qq.activity;
+
+import com.jie.ke.my.qq.R;
+
+import android.os.Bundle;
+/**
+ * 
+ * @author jieke
+ *
+ */
+public class SplashActivity extends BaseActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+}
---
+++ src/com/jieke/qq/entity/User.java
@@ -0,0 +1,41 @@
+package com.jieke.qq.entity;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1435883452106452612L;
+	private String id;
+	private String name;
+	private String age;
+	private String sex;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
