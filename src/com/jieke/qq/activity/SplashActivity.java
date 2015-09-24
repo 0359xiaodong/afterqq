729cc6521317ab994ea6048a514470c5034445ac
spadd <vimchan@qq.com>
2015-09-24 12:58:29 +0800

    增加工具类，主题

--- src/com/jieke/qq/activity/SplashActivity.java
+++ src/com/jieke/qq/activity/SplashActivity.java
@@ -1,18 +1,35 @@
 package com.jieke.qq.activity;
 
+import java.lang.ref.WeakReference;
+
 import com.jie.ke.my.qq.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 /**
  * 
  * @author jieke
  *
  */
-public class SplashActivity extends BaseActivity{
+public class SplashActivity extends BaseActivity implements Runnable {
+
+	private WeakReference<SplashActivity> activity = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+		getWindow().setBackgroundDrawableResource(R.drawable.splash);
+		activity = new WeakReference<SplashActivity>(this);
+		new Handler().postDelayed(this, 3000);
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		startActivity(new Intent(activity.get(), MainActivity.class));
+		this.finish();
 	}
 }
