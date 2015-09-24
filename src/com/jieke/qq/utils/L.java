90416a06900ba5fa0589f9b825d973350a507636
spadd <vimchan@qq.com>
2015-09-24 13:11:42 +0800

    11

---
+++ src/com/jieke/qq/utils/L.java
@@ -0,0 +1,61 @@
+package com.jieke.qq.utils;
+
+import android.util.Log;
+
+/**
+ * 打印日志工具类
+ * 
+ * @author v
+ *
+ */
+public class L {
+	public final static String TAG = "qqtag";
+	/* 是否Debug，发布请在全局关闭 */
+	public static boolean isDebug = true;
+
+	private L() {
+		throw new UnsupportedOperationException("不能实例化该对象");
+	}
+
+	// 下面四个是默认tag的函数
+	public static void i(String msg) {
+		if (isDebug)
+			Log.i(TAG, msg);
+	}
+
+	public static void d(String msg) {
+		if (isDebug)
+			Log.d(TAG, msg);
+	}
+
+	public static void e(String msg) {
+		if (isDebug)
+			Log.e(TAG, msg);
+	}
+
+	public static void v(String msg) {
+		if (isDebug)
+			Log.v(TAG, msg);
+	}
+
+	// 下面是传入自定义tag的函数
+	public static void i(String tag, String msg) {
+		if (isDebug)
+			Log.i(tag, msg);
+	}
+
+	public static void d(String tag, String msg) {
+		if (isDebug)
+			Log.i(tag, msg);
+	}
+
+	public static void e(String tag, String msg) {
+		if (isDebug)
+			Log.i(tag, msg);
+	}
+
+	public static void v(String tag, String msg) {
+		if (isDebug)
+			Log.i(tag, msg);
+	}
+}
